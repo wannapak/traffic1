dd7490820f93d83f798eb674336be4afbb4aeeb9
Pc <tabochicky@gmail.com>
2016-09-28 17:10:40 +0700

    28/9/59 newupdate

--- app/src/main/java/com/wannapak/pc/traffic/MainActivity.java
+++ app/src/main/java/com/wannapak/pc/traffic/MainActivity.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -20,7 +21,23 @@ public class MainActivity extends AppCompatActivity {
         MyAdapter objMyAdapter = new MyAdapter(MainActivity.this,intIcon,strTitle);
         ListView myListView = (ListView) findViewById(R.id.listView);
         myListView.setAdapter(objMyAdapter);
+
+        //Active When Click ListView
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                myIntentToDetail(i);
+            }
+        });
+
     } //createListView
+
+private void myIntentToDetail(int intClick) {
+    Intent objInternet = new Intent(MainActivity.this, ShowDetailActivity.class);
+    objInternet.putExtra("click", intClick);
+    startActivity(objInternet);
+}
+
     public void ClickAboutMe (View view){
         Intent objIntent = new Intent(Intent.ACTION_VIEW);
         objIntent.setData(Uri.parse("http://www.expertlab.ssru.ac.th"));
